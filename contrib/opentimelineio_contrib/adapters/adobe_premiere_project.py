1b5e1ad03868280ef70e2a06d6b7a7307cfab18e
Jonathan Hearn <jonathan.hearn@ajptechnical.com>
2020-12-09 15:14:42 +0000

    Ensure ExternalMediaReference objects have available_range set

--- contrib/opentimelineio_contrib/adapters/adobe_premiere_project.py
+++ contrib/opentimelineio_contrib/adapters/adobe_premiere_project.py
@@ -10,6 +10,7 @@ import opentimelineio as otio
 class AdobePremiereProjectParseError(otio.exceptions.OTIOError):
     pass
 
+
 def read_from_file(filepath):
     try:
         input_str = gzip.GzipFile(filepath).read()
@@ -17,12 +18,14 @@ def read_from_file(filepath):
         input_str = open(filepath).read()
     return read_from_string(input_str)
 
+
 def read_from_string(input_str):
     try:
         root = ET.fromstring(input_str)
     except ET.ParseError:
         raise AdobePremiereProjectParseError("Data is neither XML nor gzipped XML.")
     return AdobePremiereProject(root).to_collection()
+
 
 class AdobePremiereProject(object):
     def __init__(self, root):
@@ -44,20 +47,26 @@ class AdobePremiereProject(object):
         if id_ not in self._object_cache:
             node = self._root.find("*[@{}='{}']".format(attr_name, id_))
             if not node:
-                raise AdobePremiereProjectParseError("Couldn't find object with {} {}".format(attr_name, id_))
+                raise AdobePremiereProjectParseError(
+                    "Couldn't find object with {} {}".format(attr_name, id_)
+                )
             self._object_cache[id_] = node
         return self._object_cache[id_]
 
     def _dereference(self, node):
         id_ = node.get("ObjectRef")
         if not id_:
-            raise AdobePremiereProjectParseError("Node has no ObjectRef attribute: {}".format(node))
+            raise AdobePremiereProjectParseError(
+                "Node has no ObjectRef attribute: {}".format(node)
+            )
         return self._get_object(id_)
-    
+
     def _udereference(self, node):
         uid = node.get("ObjectURef")
         if not uid:
-            raise AdobePremiereProjectParseError("Node has no ObjectURef attribute: {}".format(node))
+            raise AdobePremiereProjectParseError(
+                "Node has no ObjectURef attribute: {}".format(node)
+            )
         return self._get_object(uid, "ObjectUID")
 
     def _dereference_all(self, nodes):
@@ -79,7 +88,11 @@ class AdobePremiereProject(object):
                 elif track_node.tag == "AudioClipTrack":
                     track_kind = "Audio"
                 else:
-                    raise AdobePremiereProjectParseError("Unknown track type: {} (ObjectID: {})".format(track_node.tag, track_node.get("ObjectID")))
+                    raise AdobePremiereProjectParseError(
+                        "Unknown track type: {} (ObjectID: {})".format(
+                            track_node.tag, track_node.get("ObjectID")
+                        )
+                    )
                 track = otio.schema.Track(kind=track_kind)
                 last_track_end = 0
                 for top_track_item_node in self._dereference_all(
@@ -92,7 +105,9 @@ class AdobePremiereProject(object):
                     if track_start > last_track_end:
                         track.append(
                             otio.schema.Gap(
-                                duration=otio.opentime.RationalTime(track_start - last_track_end)
+                                duration=otio.opentime.RationalTime(
+                                    track_start - last_track_end
+                                )
                             )
                         )
                     sub_clip_node = self._dereference(
@@ -100,20 +115,56 @@ class AdobePremiereProject(object):
                     )
                     top_clip_node = self._dereference(sub_clip_node.find("Clip"))
                     clip_node = top_clip_node.find("Clip")
-                    clip_in = otio.opentime.RationalTime(int(clip_node.find("InPoint").text) / 254016000000)
-                    clip_out = otio.opentime.RationalTime(int(clip_node.find("OutPoint").text) / 254016000000)
+                    clip_in = otio.opentime.RationalTime(
+                        int(clip_node.find("InPoint").text)
+                        / 254016000000
+                    )
+                    clip_out = otio.opentime.RationalTime(
+                        int(clip_node.find("OutPoint").text)
+                        / 254016000000
+                    )
                     media_source_node = self._dereference(clip_node.find("Source"))
                     media_node_ref = media_source_node.find("MediaSource/Media")
                     if media_node_ref is not None:
                         media_node = self._udereference(media_node_ref)
                         media_path = media_node.find("ActualMediaFilePath").text
+                        media_start = otio.opentime.RationalTime(
+                            int(media_node.find("Start").text)
+                            / 254016000000
+                        )
+                        if track_kind == "Video":
+                            video_stream_node = self._dereference(
+                                media_node.find("VideoStream")
+                            )
+                            media_duration = otio.opentime.RationalTime(
+                                int(video_stream_node.find("Duration").text)
+                                / 254016000000
+                            )
+                        elif track_kind == "Audio":
+                            video_stream_node = self._dereference(
+                                media_node.find("AudioStream")
+                            )
+                            media_duration = otio.opentime.RationalTime(
+                                int(video_stream_node.find("Duration").text)
+                                / 254016000000
+                            )
+                        else:
+                            raise NotImplementedError(
+                                "Can only handle Video or Audio here atm."
+                            )
                         clip = otio.schema.Clip(
                             media_reference=otio.schema.ExternalReference(
                                 target_url=media_path,
+                                available_range=otio.opentime.TimeRange(
+                                    start_time=media_start,
+                                    duration=media_duration,
+                                ),
                             ),
                         )
                     else:
-                        sequence_node_ref = media_source_node.find("SequenceSource/Sequence")
+                        sequence_node_ref = media_source_node.find(
+                            "SequenceSource/Sequence"
+                        )
                         sequence_node = self._udereference(sequence_node_ref)
                         clip = self._stack_from_sequence_node(sequence_node)
                     clip.source_range = otio.opentime.TimeRange(
